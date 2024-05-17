c359c54486f062a09e232900997317989abcea11
BiggyTech <akinyemitestimonyiadedayo@gmail.com>
2024-05-18 00:05:25 +0530

    make the H1 element text-center in the content.jsx

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,7 @@
 const Content = ()=>{
     return(
         <main className=" min-h-[90vh] gap-6 flex flex-col justify-center items-center">
-            <h1 className="font-bold">Welcome to Great Lil school CBT Exam site</h1>
+            <h1 className="font-bold text-center">Welcome to Great Lil school CBT Exam site</h1>
             <button className="bg-black rounded text-[#ffffff] px-5 py-1  hover:bg-[#000000ab] outline-double outline-2 outline-zinc-400">get started</button>
         </main>
     )
